fb17e99e3c2c0f6a45c3644df7a2c2c52e0dd19f
waheeb mahyoob <134747118+waheeb71@users.noreply.github.com>
2025-10-19 18:46:59 +0300

    Add routes for cash invoices and related forms

--- src/App.jsx
+++ src/App.jsx
@@ -4,47 +4,80 @@ import QuoteForm from "./components/QuoteForm";
 import QuotesList from "./components/QuotesList";
 import LoginPage from "./components/Login";
 import PrivateRoute from "./components/PrivateRoute";
+// *** المكونات الجديدة (قد تحتاج إلى تعديل المسارات حسب هيكل مشروعك) ***
+import CashInvoiceForm from "./components/CashInvoiceForm";
+import CashInvoicesList from "./components/CashInvoicesList"; // 💡 تأكد من استيراد هذا المكون
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        
-        <Route 
-          path="/" 
+
+        {/* المسار الرئيسي */}
+        <Route
+          path="/"
           element={
             <PrivateRoute>
               <Home />
             </PrivateRoute>
-          } 
+          }
         />
-        
-        <Route 
-          path="/new-quote" 
+
+        {/* مسارات عروض الأسعار */}
+        <Route
+          path="/new-quote"
           element={
             <PrivateRoute>
               <QuoteForm />
             </PrivateRoute>
-          } 
+          }
         />
-        
-        <Route 
-          path="/quotes" 
+        <Route
+          path="/quotes"
           element={
             <PrivateRoute>
               <QuotesList />
             </PrivateRoute>
-          } 
+          }
         />
-        
-        <Route 
-          path="/edit-quote/:quoteId" 
+        <Route
+          path="/edit-quote/:quoteId"
           element={
             <PrivateRoute>
               <QuoteForm />
             </PrivateRoute>
-          } 
+          }
+        />
+
+        {/* مسارات الفواتير النقدية الجديدة */}
+        <Route
+          path="/new-cash-invoice"
+          element={
+            <PrivateRoute>
+              <CashInvoiceForm />
+            </PrivateRoute>
+          }
+        />
+
+        {/* مسار استعراض الفواتير النقدية */}
+        <Route
+          path="/cash-invoices" // 💡 المسار الجديد لعرض القائمة
+          element={
+            <PrivateRoute>
+              <CashInvoicesList />
+            </PrivateRoute>
+          }
+        />
+
+        {/* مسار تعديل الفواتير النقدية */}
+        <Route
+          path="/edit-cash-invoice/:invoiceId" // 💡 المسار الجديد للتعديل
+          element={
+            <PrivateRoute>
+              <CashInvoiceForm />
+            </PrivateRoute>
+          }
         />
       </Routes>
     </Router>
